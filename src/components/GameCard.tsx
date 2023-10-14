88ba56a2c53c8cabf367769702949c48a40efbb8
Iqbal Ezedin <iqbalezedin5@gmail.com>
2023-10-14 13:22:12 -0700

    Dispaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Game } from './Hooks/useGames'
-import { Card, CardBody, CardHeader, Image, Text } from '@chakra-ui/react'
+import { Card, CardBody, CardHeader, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
+import CriticScore from './criticScore'
 interface props {
     game: Game
 }
@@ -11,7 +12,11 @@ const GameCard = ({game}: props) => {
         <Image src={game.background_image} />
         <CardBody>
             <CardHeader fontSize='2xl' >{game.name}</CardHeader>
+            <HStack justifyContent= 'space-between'>
             <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)} />
+            <CriticScore score={game.metacritic} />
+            </HStack>
+            
         </CardBody>
     </Card>
   )
--- src/components/Hooks/useGames.ts
+++ src/components/Hooks/useGames.ts
@@ -11,7 +11,9 @@ export interface Platform {
         id: number,
         name: string,
         background_image: string,
-        parent_platforms: {platform: Platform} []
+        parent_platforms: {platform: Platform} [],
+        metacritic: number
+
     }
     
     interface FetchGamesResponse {
