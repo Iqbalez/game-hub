69e2a0d91b67842521e911191c2fd94556962034
Iqbal Ezedin <iqbalezedin5@gmail.com>
2023-10-15 04:35:33 -0700

    creatin a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import { SimpleGrid, Text } from '@chakra-ui/react'
 
 
 const GameGrid = () => {
-    const {games, error, isLoading} = useGames();
+    const {data, error, isLoading} = useGames();
     const skeletons = [1, 2, 3, 4, 5, 6]
 
     
@@ -16,7 +16,7 @@ const GameGrid = () => {
     {error && <Text>{error}</Text>}
     <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
       {isLoading && skeletons.map(skeleton =>( <GamesCardContainer> <GameCardSkeleton key={skeleton}/></GamesCardContainer>))}
-      {games.map((game)=> (
+      {data.map((game)=> (
         <GameCard key={game.id} game={game}/>
         ))}
         </SimpleGrid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import useGenres from './Hooks/useGenre'
+
+import useGenres, { Genre } from './Hooks/useGenre'
+
 
 const GenreList = () => {
 
-const {genres} = useGenres()
+const {data} = useGenres()
 
   return (
    <ul>
-    {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+    {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
    </ul>
   )
 }
